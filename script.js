3b4108a072058b4d86832f0bf2f2a05fa6abeb79
akiemw@ <akiemiq4g@icloud.com>
2025-07-11 03:45:13 -0400

    feat: Redesign and add 6-month learning plan

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const board = document.getElementById('trello-board');
-    board.addEventListener('click', function() {
-        alert('This is my static Trello board!')
-    });
-});
